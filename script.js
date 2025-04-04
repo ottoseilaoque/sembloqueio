9be2120d8af35b6760d41b7af457fb3e02b756b7
ottoseilaoque <ottoseibtcamboa@gmail.com>
2025-04-04 08:05:37 -0300

    novos jogos adicionados

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const idJogos = document.querySelector("#jogos")
             { nome: "Jogo da Cobrinha", url: "https://www.google.com/search?q=snake", img: "https://www.harapanrakyat.com/wp-content/uploads/2022/05/Game-Snake-Google-Maps-yang-Seru-Simak-Cara-untuk-Memainkannya-1024x576.jpg" },
             { nome: "Run 3", url: "https://player03.com/run/3/beta", img: "https://player03.com/run/3/beta/favicon.png" },
             { nome: "Minecraft", url: "https://eaglercraft.com/#google_vignette", img: "https://wallpapers.com/images/hd/minecraft-logo-with-basic-block-structure-03dch9gg2iy4hthk.jpg" },
+            { nome: "Geometry Dash", url: "https://geometrydashlite.io/geometry-dash-lite", img: "https://wallpapers.com/images/hd/minecraft-logo-with-basic-block-structure-03dch9gg2iy4hthk.jpg" },
         ]
         
         informacoesJogos.forEach(item => {
