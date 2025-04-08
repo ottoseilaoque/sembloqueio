d5f064f004310ed796ff0373b6f3dd9b8603c990
ottoseilaoque <ottoseibtcamboa@gmail.com>
2025-04-08 09:40:41 -0300

    script.js
    Arumando bugs e colocando mais jogos

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const idJogos = document.querySelector("#jogos")
             { nome: "Coreball", url: "https://www.onlinescientificresearch.com/games/core-ball/", img: "https://doodle-jump.co/data/image/game/coreball.jpg" },
             { nome: "Mario All Stars", url: "https://www.smbgames.be/super-mario-all-stars.php", img: "https://i.ytimg.com/vi/SNl6WJygciw/maxresdefault.jpg" },
             { nome: "Monkey mart", url: "https://inzoiplay.com/monkey-market/", img: "https://th.bing.com/th/id/OIP.p-YmWD4Xidvel4SQocIxdwHaHp?rs=1&pid=ImgDetMain" },
+            { nome: "Pac man", url: "https://www.onlinescientificresearch.com/games/pac-man/", img: "https://i.guim.co.uk/img/media/8152f8ea7f06fd8ef5c68a3a594e6ac35dfd774b/0_342_800_480/master/800.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=42bb412858826e59cd33e40975ca3ee1" },
         ]
         
         informacoesJogos.forEach(item => {
