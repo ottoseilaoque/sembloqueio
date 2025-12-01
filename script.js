dbc9ef37c48c59f9e8b83f0b44dcf21be3e62988
ottoseilaoque <ottoseibtcamboa@gmail.com>
2025-12-01 08:25:33 -0300

    script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const idJogos = document.querySelector("#jogos")
             { nome: "Az Games", url: "https://azgames.io/", img: "https://img.sur.ly/favicons/a/azgames.io.ico" },
             { nome: "Jogos Friv", url: "https://jogosfriv.com.br/", img: "https://jogosfriv.com.br/favicon.png" },
             { nome: "Friv2Online", url: "https://www.friv2online.com/pt", img: "https://www.friv2online.com/favicon.png" },
+            { nome: "Flash Legacy", url: "https://www.flashlegacy.com/", img: "https://www.flashlegacy.com/favicon.ico" },
             { nome: "Google Feud", url: "https://googlefeud.com/", img: "https://i.ytimg.com/vi/xC-oO1mJUGY/maxresdefault.jpg" },
             { nome: "Conexo", url: "https://conexo.ws/pt/", img: "https://th.bing.com/th/id/OIP.wdpL07KYYRzUFSWmkm6dygHaE8?r=0&rs=1&pid=ImgDetMain" },
             { nome: "termo", url: "https://termo.pt/", img: "https://play-lh.googleusercontent.com/HMqznNZmnuR2wmipfZUaPcqrZnkWT9xOoV7QepkuMM5F7NiMviYzqmC-IWlzwFbUew" },
