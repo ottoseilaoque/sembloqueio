023ce2742a810026da7f972d1ed007cdcbc955c7
ottoseilaoque <ottoseibtcamboa@gmail.com>
2026-05-20 10:59:35 -0300

    script.js

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ const idJogos = document.querySelector("#jogos")
             { nome: "Pokémon Color", url: "https://www.pokemonpalette.com/game", img: "https://tse4.mm.bing.net/th/id/OIP.UYbN9lLdpKd5o1XvmObR_gHaHa?w=600&h=600&rs=1&pid=ImgDetMain&o=7&rm=3" },
             { nome: "Flappy Bird", url: "https://flappybird.guru/pt/index.html", img: "https://www.pngmart.com/files/12/Flappy-Bird-PNG-Image.png" },
             { nome: "Gênio Quiz", url: "https://www.genioquiz.com.br/genio-quiz-1/", img: "https://genioquiz.com.br/wp-content/uploads/2022/05/Logo-Genio-Quiz.png" },
-            { nome: "Pokelike", url: "https://pokelike.xyz/po", img: "https://tse1.mm.bing.net/th/id/OIP.-DNav8VsKmZcpwDAwkpopQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" },
+            { nome: "Pokelike", url: "https://pokelike.xyz", img: "https://tse1.mm.bing.net/th/id/OIP.-DNav8VsKmZcpwDAwkpopQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" },
     
         ]
     arrayFinal = informacoesJogos
