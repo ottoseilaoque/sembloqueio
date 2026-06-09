89f5a02021fa4d80c5f2c1999686ff2780512862
ottoseilaoque <ottoseibtcamboa@gmail.com>
2026-06-09 08:37:02 -0300

    script.js

--- script.js
+++ script.js
@@ -116,6 +116,7 @@ const idJogos = document.querySelector("#jogos")
             { nome: "Quenq", url: "https://quenq.com/", img: "https://quenq.com/og.jpg" },
             { nome: "Oasis", url: "https://oasis.decart.ai/welcome", img: "https://tse3.mm.bing.net/th/id/OIP.Omkqx6B7eb6f3FE_AAdDugHaEK?rs=1&pid=ImgDetMain&o=7&rm=3" },
             { nome: "Jogos do g1", url: "https://g1.globo.com/jogos/", img: "https://tse2.mm.bing.net/th/id/OIP.L_iImcL-1TXh2jPEQfQ28gAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" },
+            { nome: "Pokennection", url: "https://www.pokennection.com/", img: "https://pngimg.com/uploads/pokeball/pokeball_PNG21.png" },
             
         ]
     arrayFinal = informacoesJogos
